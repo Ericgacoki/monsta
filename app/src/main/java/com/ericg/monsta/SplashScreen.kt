b032dc72d48797f1b7d3e974723fd2503418d1df
Ericgacoki <gacokieric@gmail.com>
2021-04-29 17:11:18 +0300

    implemented data binding
    Signed-off-by: Ericgacoki <gacokieric@gmail.com>

---
+++ app/src/main/java/com/ericg/monsta/SplashScreen.kt
@@ -0,0 +1,17 @@
+package com.ericg.monsta
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ericg.monsta.databinding.SplashScreenBinding
+
+class SplashScreen : AppCompatActivity() {
+    private var _binding: SplashScreenBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        _binding = SplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
