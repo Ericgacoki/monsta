32aec8e35bb5508f5de8669c4c42bf7c61997489
Ericgacoki <gacokieric@gmail.com>
2022-01-05 05:18:38 +0300

    get the binding

--- app/src/main/java/com/ericg/monsta/SplashScreen.kt
+++ app/src/main/java/com/ericg/monsta/SplashScreen.kt
@@ -6,12 +6,12 @@ import com.ericg.monsta.databinding.SplashScreenBinding
 
 class SplashScreen : AppCompatActivity() {
     private var _binding: SplashScreenBinding? = null
-    private val binding get() = _binding!!
+    private val binding get() = _binding!! // get the binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide() // hide the title bar
-        
+
         _binding = SplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
